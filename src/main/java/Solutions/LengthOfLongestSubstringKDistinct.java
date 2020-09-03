bcd614d786fb269ed08b022f56b6d9e6f8eda64f
pwzlyx <452349552@qq.com>
2020-09-03 19:38:00 +0800

    340. 至多包含 K 个不同字符的最长子串 滑动窗口

--- src/main/java/Solutions/LengthOfLongestSubstringKDistinct.java
+++ src/main/java/Solutions/LengthOfLongestSubstringKDistinct.java
@@ -1,8 +1,33 @@
 package Solutions;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LengthOfLongestSubstringKDistinct {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        
-        return 0;
+        char[] chars = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<Character, Integer>(k);
+        int start = 0;
+        int end = 0;
+        int max = 1;
+        for (int i = 0; i < chars.length; i++){
+            end++;
+            char nowChar = chars[end];
+            map.put(nowChar, end);
+            if (map.size() > k){
+               int hashValue = Collections.min(map.values());
+               start = hashValue + 1;
+               map.remove(chars[hashValue]);
+            }
+        }
+        max = Math.max(max, end-start+1);
+        return max;
+    }
+
+    public static void main(String[] args) {
+        LengthOfLongestSubstringKDistinct lengthOfLongestSubstringKDistinct = new LengthOfLongestSubstringKDistinct();
+        int res = lengthOfLongestSubstringKDistinct.lengthOfLongestSubstringKDistinct("abbba", 1);
+        System.out.println(res);
     }
 }
